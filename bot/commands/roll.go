5ed0ed544a2d2f63de0d628c00087db66ccc70ff
Stephen <smyoung30@yahoo.com>
2025-07-18 14:10:08 -0400

    improvements

--- bot/commands/roll.go
+++ bot/commands/roll.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"regexp"
 	"strconv"
-	"time"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -14,20 +15,43 @@ func roll(c *Command, s *discordgo.Session, m *discordgo.MessageCreate) error {
 	var (
 		args = c.Args()
 	)
-	args = nil
 
 	if len(args) < 1 {
 		MessageSend(s, m, rollDuality())
 		return nil
 	}
 
-	diceNum, err := strconv.ParseFloat(args[0], 64)
-	if err != nil {
-		MessageSend(s, m, "Need to roll a number or duality")
-		return nil
+	var results []string
+
+	for _, roll := range args {
+
+		if roll == "duality" || roll == "duelity" {
+			results = append(results, rollDuality())
+			continue
+		}
+
+		isMultiDiceRoll, _ := regexp.MatchString("^[0-9]*d[0-9]+.*$", roll)
+
+		diceNum, err := strconv.ParseFloat(roll, 64)
+		if err != nil && !isMultiDiceRoll {
+			results = append(results, fmt.Sprintf("%s is not a valid roll. A roll is a number, duality, or dice abbreviation", roll))
+			continue
+		}
+		if isMultiDiceRoll {
+			results = append(results, multiDiceRoll(roll))
+		} else {
+			_, diceRollResultString := rollDice(diceNum)
+
+			if diceRollResultString == "1" {
+				results = append(results, fmt.Sprintf("your d%s result is %s :cry:\n", roll, diceRollResultString))
+				continue
+			}
+
+			results = append(results, fmt.Sprintf("your d%s result is %s\n", roll, diceRollResultString))
+		}
 	}
-	diceRollResult := int(math.Ceil(rand.Float64() * diceNum))
-	MessageSend(s, m, fmt.Sprintf("%o", diceRollResult))
+
+	MessageSend(s, m, strings.Join(results, "\n"))
 	return nil
 
 }
@@ -38,8 +62,12 @@ func init() {
 
 func rollDuality() string {
 
-	hope := int(math.Ceil(rand.Float64() * 12))
-	fear := int(math.Ceil(rand.Float64() * 12))
+	hope, hopeString := rollDice(12)
+	fear, fearString := rollDice(12)
+
+	result := hope + fear
+	resultString := strconv.FormatFloat(result, 'f', -1, 64)
+
 	var dualityResult string
 
 	if hope > fear {
@@ -47,14 +75,51 @@ func rollDuality() string {
 	} else if fear > hope {
 		dualityResult = "You rolled with Fear :dagger:"
 	} else {
-		dualityResult = "YOU CRIT!!!! :dagger: :heart:"
+		dualityResult = "# YOU CRIT!!!! :dagger: :heart:"
 	}
 
-	return fmt.Sprintf(">>> %s \nYour Hope roll was %o \nYour Fear roll was %o \nYour roll was %o\n %f", dualityResult, hope, fear, hope+fear, (rand.Float64() * 12))
+	return fmt.Sprintf("> %s \n> Your Hope roll was %s \n> Your Fear roll was %s \n> Your total was %s\n", dualityResult, hopeString, fearString, resultString)
 
 }
 
-func rollDice(num int) int {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	return (r.Intn(num) + 1)
+func rollDice(diceSides float64) (float64, string) {
+	diceRollResult := math.Ceil(rand.Float64() * diceSides)
+	diceRollResultString := strconv.FormatFloat(diceRollResult, 'f', -1, 64)
+	return diceRollResult, diceRollResultString
+}
+
+func multiDiceRoll(diceDesignation string) string {
+	var positiveModifier float64 = 0
+	var negativeModifier float64 = 0
+	var diceTotal float64 = 0
+	var diceDesignationString = diceDesignation
+
+	if strings.Contains(diceDesignation, "+") {
+		modifierSplitStrings := strings.Split(diceDesignation, "+")
+		positiveModifier, _ = strconv.ParseFloat(modifierSplitStrings[1], 64)
+		diceDesignation = modifierSplitStrings[0]
+	} else if strings.Contains(diceDesignation, "-") {
+		modifierSplitStrings := strings.Split(diceDesignation, "-")
+		negativeModifier, _ = strconv.ParseFloat(modifierSplitStrings[1], 64)
+		diceDesignation = modifierSplitStrings[0]
+	}
+
+	splitString := strings.Split(diceDesignation, "d")
+	numRolls, err := strconv.Atoi(splitString[0])
+	if err != nil {
+		numRolls = 1
+	}
+	rollValue, err := strconv.ParseFloat(splitString[1], 64)
+	if err != nil {
+
+	}
+	for i := 0; i < numRolls; i++ {
+		dicevalue, _ := rollDice(rollValue)
+		diceTotal += dicevalue
+	}
+
+	diceTotal += (positiveModifier - negativeModifier)
+	diceTotalString := strconv.FormatFloat(diceTotal, 'f', -1, 64)
+
+	return fmt.Sprintf("your %s result is %s\n", diceDesignationString, diceTotalString)
 }
